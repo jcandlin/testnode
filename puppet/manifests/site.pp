b699125d8676360aafb9413e332844c9760095bb
jcandlin <jon.candlin@gmail.com>
2014-07-23 15:33:28 +0100

    Added java7-jre-headless

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -2,8 +2,12 @@ Exec {
     path => ["/bin/", "/sbin/", "/usr/bin/", "/usr/sbin/"]
 }
 
+exec    { 'apt-get update':
+        command => '/usr/bin/apt-get update'
+}
+
 node testnode {
 	include bootstrap
-    include tomcat
     include tools
+    include tomcat
 }
